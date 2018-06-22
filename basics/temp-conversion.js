a8104539472b1cc8dcb3897632c06357cf1bc37b
Jens Kai <jkaih7@gmail.com>
2018-06-21 22:09:03 -0400

    temp converter done

---
+++ basics/temp-conversion.js
@@ -0,0 +1,10 @@
+let fahrenheit = 32;
+
+let celcius = (fahrenheit - 32) * 5/9;
+
+console.log(celcius);
+
+let kelvinBeforeRound = (fahrenheit + 459.67) * 5/9;
+let kelvin = Math.round(kelvinBeforeRound);
+
+console.log(kelvin);
