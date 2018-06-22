194a71b90bf261b82b96f1a1e479121c0e45e1f0
Jens Kai <jkaih7@gmail.com>
2018-06-21 20:50:28 -0400

    numbers complete

---
+++ basics/numbers.js
@@ -0,0 +1,19 @@
+// let x = 11
+// let num = (x + 1) * 2;
+
+// console.log(num);
+
+// let age = 31;
+
+// let dogYears = (age + 1) /7;
+
+// console.log(dogYears);
+
+//challenge == 2
+
+let studentScore = 18;
+let maxScore = 20;
+
+let percent = (studentScore / maxScore) * 100;
+
+console.log(percent + '%');
