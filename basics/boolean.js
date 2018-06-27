27de6e1e790519813f30a549f23314c2d1ae9f22
Jens Kai <jkaih7@gmail.com>
2018-06-26 21:16:43 -0400

    if statements

--- basics/boolean.js
+++ basics/boolean.js
@@ -1,19 +1,26 @@
-// let temp = 32;
-// let isFreezing = temp <= 32;
+let temp = 33;
+let isFreezing = temp <= 32;
+
+if (isFreezing) {
+    console.log("It's FREEZING outside!");
+}else{
+   console.log("It's not freezing");
+}
 
 // console.log(isFreezing);
 
 //==challenge==
 
 //create age variable set to your age
-let age = 31;
-let isChild = age <= 7;
-let isSenior = age >= 65;
-let isAdult = age > 7 && age < 65;
 
-console.log("Am I a Child? :" + isChild);
-console.log("Am I a Senior? :" + isSenior);
-console.log("Am I an Adult? :" + isAdult);
+// let age = 31;
+// let isChild = age <= 7;
+// let isSenior = age >= 65;
+// let isAdult = age > 7 && age < 65;
+
+// console.log("Am I a Child? :" + isChild);
+// console.log("Am I a Senior? :" + isSenior);
+// console.log("Am I an Adult? :" + isAdult);
 
 //is child 7 or under
 //is senior 65 or older
