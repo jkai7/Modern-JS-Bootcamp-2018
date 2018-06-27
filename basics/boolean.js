3cc942a5eba7221606580e0139e7cb6b41b3b517
Jens Kai <jkaih7@gmail.com>
2018-06-26 21:05:26 -0400

    booleans done

--- basics/boolean.js
+++ basics/boolean.js
@@ -1,3 +1,20 @@
-let isFreezing = false
+// let temp = 32;
+// let isFreezing = temp <= 32;
 
-console.log(isFreezing);
+// console.log(isFreezing);
+
+//==challenge==
+
+//create age variable set to your age
+let age = 31;
+let isChild = age <= 7;
+let isSenior = age >= 65;
+let isAdult = age > 7 && age < 65;
+
+console.log("Am I a Child? :" + isChild);
+console.log("Am I a Senior? :" + isSenior);
+console.log("Am I an Adult? :" + isAdult);
+
+//is child 7 or under
+//is senior 65 or older
+//print both values
