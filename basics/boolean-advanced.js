00dc57a1b815b835069c9b12833e581c5b94f62f
Jens Kai <jkaih7@gmail.com>
2018-06-29 20:26:34 -0400

    else if challenge complete

--- basics/boolean-advanced.js
+++ basics/boolean-advanced.js
@@ -1,10 +1,25 @@
-let isAccountLocked = false 
-let userRole = "admin"
+// let isAccountLocked = false 
+// let userRole = "admin"
 
-if (isAccountLocked) {
-    console.log("Account is locked");
-} else if (userRole === "admin") {
-    console.log("welcome admin");
-} else {
-    console.log("welcome");
+// if (isAccountLocked) {
+//     console.log("Account is locked");
+// } else if (userRole === "admin") {
+//     console.log("welcome admin");
+// } else {
+//     console.log("welcome");
+// }
+
+//challenge 
+
+let temp = 45;
+let isFreezing = temp <= 32;
+let isHot = temp >= 100;
+
+
+if (isFreezing) {
+    console.log("It's FREEZING!");
+}else if (isHot) {
+   console.log("It's HOT outside!");
+}else{
+    console.log("it's nice outside");
 }
