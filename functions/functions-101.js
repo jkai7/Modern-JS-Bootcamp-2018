7df96a39ca830ab9037c476b2ef62fc6c2d93ba0
Jens Kai <jkaih7@gmail.com>
2018-07-02 17:59:22 -0400

    function challenge complete

--- functions/functions-101.js
+++ functions/functions-101.js
@@ -1,16 +1,31 @@
 //function = input (argument(s)), code, output (return value)
 
-let greetUser = function () {
-    console.log('Welcome user!')
+// let greetUser = function () {
+//     console.log('Welcome user!')
+// }
+
+// greetUser();
+
+// let square = function (num) {
+//     let result = num *num;
+//     return result;
+// }
+
+// let value = square(3);
+
+// console.log(value)
+
+//challenge
+//convert fahrenheit to celcius
+
+let conversion = function (degreesInFahrenheit) {
+    //T(°C) = (T(°F) - 32) × 5/9
+    let celsius = (degreesInFahrenheit - 32) * 5/9
+    return celsius
 }
 
-greetUser();
+let degreesInCelsius = conversion(32);
+let degreesInCelsius2 = conversion(68);
 
-let square = function (num) {
-    let result = num *num;
-    return result;
-}
-
-let value = square(3);
-
-console.log(value)
+console.log(degreesInCelsius);
+console.log(degreesInCelsius2);
