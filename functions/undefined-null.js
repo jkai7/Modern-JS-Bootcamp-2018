6eaaf584e02d4266ddf53130b6b5a01424ec8ac2
Jens Kai <jkaih7@gmail.com>
2018-07-02 18:16:26 -0400

    undefined and null complete

---
+++ functions/undefined-null.js
@@ -0,0 +1,27 @@
+//undefined for variable
+
+let name
+
+name = 'Jens'
+
+if (name === undefined) {
+    console.log("please provide a name")
+} else {
+    console.log(name);
+}
+
+//undefined for function arguments
+
+let square = function (num) {
+    console.log(num)
+}
+
+let result = square();
+console.log(result)//undefined as function return value
+
+//
+let age = 31
+
+age = null //explicity cleared 
+
+console.log(age);
