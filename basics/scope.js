5aee67dd6bab1eb9d948890e74ce0096428c1d7d
Jens Kai <jkaih7@gmail.com>
2018-07-02 17:13:02 -0400

    scope done

--- basics/scope.js
+++ basics/scope.js
@@ -1,14 +1,37 @@
 //js uses lexical(static) scoping
 
 //global scope - contains all things defined outside all code blocks
-//local scope - defined inside code block
+    //local scope - defined inside code block
 
-let varOne = 'varOne';
+// let varOne = 'varOne';//global variable
 
-if (true) {
-    console.log(varOne);
-    let varTwo  = 'varTwo'
-    console.log(varTwo);
-}
+// if (true) {
+//     console.log(varOne);
+//     let varTwo  = 'varTwo'// local scope variable
+//     console.log(varTwo);
+
+//     if (true) {
+//         let varFour = 'varFour';
+//     }
+// }
+
+// if (true) {
+//     let varThree = 'varThree'
+// }
 
 // console.log(varTwo);
+
+// let name = 'Jens'
+
+if (true) {
+    // let name = 'Hans'// variable shadowing *** overides parent variable
+    if (true) {
+         let name = 'Jen'//leaked global w/o let or const - prevent leaked by declaring variable(let or const)
+        console.log(name);
+    }
+}
+
+if (true) {
+    console.log(name);//can only access global variable
+}
+
