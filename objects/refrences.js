d7262c16649f6d97d871f0b10a679a3d3822a345
Jens Kai <jkaih7@gmail.com>
2018-07-09 17:46:17 -0400

    refrences done

---
+++ objects/refrences.js
@@ -0,0 +1,15 @@
+let myAccount = {
+    name: 'Jens Kai',
+    expenses: 0,
+    income: 0
+}
+
+let addExpense = function (account, amount) {
+    
+    account.expenses = account.expenses + amount
+
+}
+
+addExpense(myAccount, 2.50);
+
+console.log(myAccount)
