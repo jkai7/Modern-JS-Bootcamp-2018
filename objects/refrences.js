bf91c4040a61b4535639767d9fd2a25515dbb67e
Jens Kai <jkaih7@gmail.com>
2018-07-10 18:03:02 -0400

    expense tracker comence

--- objects/refrences.js
+++ objects/refrences.js
@@ -13,3 +13,17 @@ let addExpense = function (account, amount) {
 addExpense(myAccount, 2.50);
 
 console.log(myAccount)
+
+//challenge
+
+//3 functions - add income(account, amount), 
+//reset account to zero(account), 
+//get account summary(current balance, expenses and income )
+    // account for jens has $900. $1000 in income. $100 in expenses
+
+//add income
+//add expense
+//add expense
+//get account HTMLSummaryElement
+//reset account
+//get account summary
