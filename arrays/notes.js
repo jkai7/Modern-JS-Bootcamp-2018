486a7b69f8d52b5b116fe40f2edc9db4920141ec
Jens Kai <jkaih7@gmail.com>
2018-07-17 19:48:29 -0400

    array intro challenge complete

---
+++ arrays/notes.js
@@ -0,0 +1,23 @@
+// const notes = ['note 1', 'note 2', 'note 3']
+
+// console.log(notes)
+//     //=> ['note 1', 'note 2', 'note 3']
+// console.log(notes.length)
+//     //=> 3
+// console.log(notes[1])
+//     //=> note 2
+// console.log(notes[notes.length - 1])
+    //=> access last item of array (note 3)
+
+//=> Challenge ======================================
+    //=> create an array w/ 5 to-do's
+    //=> You have x to-do's
+    //=> print first and second to last items
+        //=> To-do: walk the dog
+
+const todoList = ['walk the dog', 'brush teeth', 'do homework', 'shop for groceries', 'wash clothes']
+
+console.log(`you have ${todoList.length} things to do today`)
+console.log(`Todo: ${todoList[0]}`)
+console.log(`Todo: ${todoList[todoList.length - 2]}`)
+
