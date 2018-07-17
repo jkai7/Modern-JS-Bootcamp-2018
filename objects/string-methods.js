e2607043aa46568d249f322ace05f1e0c4054332
Jens Kai <jkaih7@gmail.com>
2018-07-16 20:08:25 -0400

    string methods

---
+++ objects/string-methods.js
@@ -0,0 +1,14 @@
+let name = 'Jens Kai'
+
+//=> length property
+console.log(name.length);
+
+//=> convert to uppercase
+console.log(name.toUpperCase());
+
+//=> convert to lower case
+console.log(name.toLowerCase());
+
+//=> includes method
+let password = 'abc123'
+console.log(password.includes('password'));
