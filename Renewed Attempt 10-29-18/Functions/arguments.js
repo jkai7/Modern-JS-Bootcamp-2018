0cd36f1a4b6de1b8b17d2d9d374f213aa017628a
Jens Kai <jkaih7@gmail.com>
2018-10-29 23:09:27 -0400

    arguments done

--- Renewed Attempt 10-29-18/Functions/arguments.js
+++ Renewed Attempt 10-29-18/Functions/arguments.js
@@ -9,9 +9,18 @@ console.log(result);
 
 // defaults arguments
 let getScore = function (name = 'player1', score = 0) {
-    return "name: " + name + " score: " + score
+    return "name: " + name + " - score: " + score
 };
 
-let scoreText = getScore();
+let scoreText = getScore(undefined, 99);
 console.log(scoreText);
 
+// challenge - tip calculator
+
+let tip = function (total, tipPercent = 0.2) {
+    return total * tipPercent 
+};
+
+console.log(tip(100));
+console.log(tip(100, 0.1));
+console.log(tip(100, 0.3));
