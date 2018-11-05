d66f5a486354d0e9e1a0e5cdf1701d10eb8c52b1
Jens Kai <jkaih7@gmail.com>
2018-11-05 14:19:56 -0500

    Array basics comnplete

---
+++ Renewed Attempt 10-29-18/Arrays/notes.js
@@ -0,0 +1,23 @@
+// Lesson Section
+const notes = ["Note 1", "Note 2", "Note 3"];
+
+// Basics ---------
+notes.length //= gives length of array
+notes[1]; //= gives second item in array
+notes[notes.length - 1]; //= gives last item in array
+
+// Manipulation ----------
+    // Methods
+
+        // PUSH and POP manipulate END of array
+notes.push("My new note"); //= adds item to end of array
+notes.pop(); //= removes last item from array
+
+        // SHIFT and UNSHIFT manipulate BEGINNING of array
+notes.shift(); // Removes first item 
+notes.unshift("My first note"); // Adds item to beginning of array
+
+        // SPLICE manipulates MIDDLE of array
+notes.splice(1, 1, "New note") // First arg. is where youre trying to start the action, second - is how many things you want to remove, third - is what you want to add
+
+    
---
+++ Renewed Attempt 10-29-18/Arrays/todo.js
@@ -0,0 +1,9 @@
+// Challenge Section
+const todo = ["Buy pet food", "Walk the dog", "Study", "Exercise", "Wash clothes"]
+
+// Challenge 1 - print you have "X" todos, first and second to last items
+
+    // console.log(`You have ${todo.length} things to do.`);
+    // console.log(`Todo #1: ${todo[0]} and Todo #4: ${todo[todo.length - 2]}`);
+
+// Challenge 2 - 
