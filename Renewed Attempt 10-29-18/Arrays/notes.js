8d7fc232b07d0a750604ec3fcf58ac547274196d
Jens Kai <jkaih7@gmail.com>
2018-11-07 14:02:19 -0500

    Organize notes

--- Renewed Attempt 10-29-18/Arrays/notes.js
+++ Renewed Attempt 10-29-18/Arrays/notes.js
@@ -2,46 +2,53 @@
 const notes = ["Note 1", "Note 2", "Note 3"];
 
 // Basics ----------------------------------------------
-notes.length //= gives length of array
-notes[1]; //= gives second item in array
-notes[notes.length - 1]; //= gives last item in array
+        notes.length //= gives length of array
+        notes[1]; //= gives second item in array
+        notes[notes.length - 1]; //= gives last item in array
 
 // Manipulation ----------------------------------------
-    // Methods
-
+    // Methods()
         // PUSH and POP manipulate END of array
-notes.push("My new note"); //= adds item to end of array
-notes.pop(); //= removes last item from array
+        notes.push("My new note"); //= adds item to end of array
+        notes.pop(); //= removes last item from array
 
         // SHIFT and UNSHIFT manipulate BEGINNING of array
-notes.shift(); // Removes first item 
-notes.unshift("My first note"); // Adds item to beginning of array
+        notes.shift(); // Removes first item 
+        notes.unshift("My first note"); // Adds item to beginning of array
 
         // SPLICE manipulates MIDDLE of array
-notes.splice(1, 1, "New note") // First arg. is where youre trying to start the action, second - is how many things you want to remove, third - is what you want to add
+        notes.splice(1, 1, "New note"); // First arg. is where you start the action, second - is how many things you want to remove, third - is what you want to add
 
 // Change value of given item with bracket notation
-notes[2] = "This is now the new Note 3"
+        notes[2] = "This is now the new Note 3";
 
 // Looping ---------------------------------------------
-notes.forEach(function (item, index) {
-    
-}) // takes function as an argument, it is a CALLBACK FUNCTION - first arg. is the item, second arg. is the index(position of item)
+        notes.forEach(function (item, index) {
+        
+        }); // takes function as an argument, it is a CALLBACK FUNCTION - first arg. is the item, second arg. is the index(position of item)
 
 // FOR Loop --------------------------------------------
-for (let count = 0; count <= 2; count++) {
-    // console.log(count);
-}; // run loop specific number of times - 1st: initializer (number in which to start loop), 2nd: condition (how long loop should run), 3rd: final expression (modify the count)
+        for (let count = 0; count <= 2; count++) {
+        // console.log(count);
+        }; // run loop specific number of times - 1st: initializer (number in which to start loop), 2nd: condition (how long loop should run), 3rd: final expression (modify the count)
 
-for (let count = 2; count >= 0; count--) {
-    
-};// loop in reverse
+        for (let count = 2; count >= 0; count--) {
+        
+        };// loop in reverse
 
-for (let count = 0; count < notes.length; count++) {
-    //console.log(notes[count]); // COUNT BECOMES THE INDEX(individual item) and we grab the item with its index using BRACKET NOTATION
-}; // loop through array
+        for (let count = 0; count < notes.length; count++) {
+        //console.log(notes[count]); // COUNT BECOMES THE INDEX(individual item) and we grab the item with its index using BRACKET NOTATION
+        }; // loop through array
 
-for (let count = notes.length -1; count >= 0; count--) {
-    //console.log(notes[count]); // count becomes index and we grab the item with its index using bracket notation
-}; // loop through array in reverse
+        for (let count = notes.length -1; count >= 0; count--) {
+        //console.log(notes[count]); // count becomes index and we grab the item with its index using bracket notation
+        }; // loop through array in reverse
+
+// SEARCHING Arrays PART 1 -----------------------------
+
+// FILTERING -------------------------------------------
+
+// SORTING ---------------------------------------------
+
+
     
--- Renewed Attempt 10-29-18/Arrays/todo.js
+++ Renewed Attempt 10-29-18/Arrays/todo.js
@@ -1,7 +1,7 @@
 // Challenge Section
-const todo = ["Buy pet food", "Walk the dog", "Study", "Exercise", "Wash clothes"]
+const todo = ["Buy pet food", "Walk the dog", "Study", "Exercise", "Wash clothes"];
 
-// Challenge #1 - print you have "X" todos, first and second to last items
+// Challenge #1 - print 'you have "X" todos', first and second to last items
 
     // console.log(`You have ${todo.length} things to do.`);
     // console.log(`Todo #1: ${todo[0]} and Todo #4: ${todo[todo.length - 2]}`);
@@ -14,14 +14,21 @@ const todo = ["Buy pet food", "Walk the dog", "Study", "Exercise", "Wash clothes
 
 // Challenge #3 - use for each, 1. first item, 2. second item, etc
 
-todo.forEach(function (item, index) {
-    const position = index + 1;
-    // console.log(`${position}. ${item}`);
-});
+    todo.forEach(function (item, index) {
+        const position = index + 1;
+        // console.log(`${position}. ${item}`);
+    });
 
 // Challenge #4 - re-create forEach outcome with a for loop
 
-for (let count = 0; count < todo.length; count++) {
-    //console.log(`${count + 1}. ${todo[count]}`);
-};
+    for (let count = 0; count < todo.length; count++) {
+        //console.log(`${count + 1}. ${todo[count]}`);
+    };
+
+// Challenge #5 - 
+
+// Challenge #6 - 
+
+// Challenge #7 - 
+
     
