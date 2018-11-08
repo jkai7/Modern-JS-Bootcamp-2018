1cd1bbafe99f7e1ad7bc4cafecee9861e3446970
Jens Kai <jkaih7@gmail.com>
2018-11-07 20:27:12 -0500

    filtering arrays challenge complete

--- Renewed Attempt 10-29-18/Arrays/notes.js
+++ Renewed Attempt 10-29-18/Arrays/notes.js
@@ -1,52 +1,121 @@
-// Lesson Section
-const notes = ["Note 1", "Note 2", "Note 3"];
+// LESSON SECTION
+const notes = [{
+        title: "My next trip",
+        body:  "I want to go back to Rhode Island"
+}, {
+        title: "Habbits to work on",
+        body: "Exercise, Eat Better, Study Harder"
+}, {
+        title: "Office Modifications",
+        body: "Get a new seat"
+}];
 
-// Basics ----------------------------------------------
-        notes.length //= gives length of array
-        notes[1]; //= gives second item in array
-        notes[notes.length - 1]; //= gives last item in array
+// // BASICS ----------------------------------------------
+//         notes.length //= gives length of array
+//         notes[1]; //= gives second item in array
+//         notes[notes.length - 1]; //= gives last item in array
 
-// Manipulation ----------------------------------------
-    // Methods()
-        // PUSH and POP manipulate END of array
-        notes.push("My new note"); //= adds item to end of array
-        notes.pop(); //= removes last item from array
+// // MANIPULATION ----------------------------------------
+//     // METHODS()
+//         // PUSH and POP manipulate END of array
+//         notes.push("My new note"); //= adds item to end of array
+//         notes.pop(); //= removes last item from array
 
-        // SHIFT and UNSHIFT manipulate BEGINNING of array
-        notes.shift(); // Removes first item 
-        notes.unshift("My first note"); // Adds item to beginning of array
+//         // SHIFT and UNSHIFT manipulate BEGINNING of array
+//         notes.shift(); // Removes first item 
+//         notes.unshift("My first note"); // Adds item to beginning of array
 
-        // SPLICE manipulates MIDDLE of array
-        notes.splice(1, 1, "New note"); // First arg. is where you start the action, second - is how many things you want to remove, third - is what you want to add
+//         // SPLICE manipulates MIDDLE of array
+//         notes.splice(1, 1, "New note"); // First arg. is where you start the action, second - is how many things you want to remove, third - is what you want to add
 
-// Change value of given item with bracket notation
-        notes[2] = "This is now the new Note 3";
+//         // Change value of given item with bracket notation
+//         notes[2] = "This is now the new Note 3";
 
-// Looping ---------------------------------------------
-        notes.forEach(function (item, index) {
+// // LOOPING ---------------------------------------------
+//         notes.forEach(function (item, index) {
         
-        }); // takes function as an argument, it is a CALLBACK FUNCTION - first arg. is the item, second arg. is the index(position of item)
+//         }); // takes function as an argument, it is a CALLBACK FUNCTION - first arg. is the item, second arg. is the index(position of item)
 
-// FOR Loop --------------------------------------------
-        for (let count = 0; count <= 2; count++) {
-        // console.log(count);
-        }; // run loop specific number of times - 1st: initializer (number in which to start loop), 2nd: condition (how long loop should run), 3rd: final expression (modify the count)
+// // FOR Loop --------------------------------------------
+//         for (let count = 0; count <= 2; count++) {
+//         // console.log(count);
+//         }; // run loop specific number of times - 1st: initializer (number in which to start loop), 2nd: condition (how long loop should run), 3rd: final expression (modify the count)
 
-        for (let count = 2; count >= 0; count--) {
+//         for (let count = 2; count >= 0; count--) {
         
-        };// loop in reverse
+//         };// loop in reverse
 
-        for (let count = 0; count < notes.length; count++) {
-        //console.log(notes[count]); // COUNT BECOMES THE INDEX(individual item) and we grab the item with its index using BRACKET NOTATION
-        }; // loop through array
+//         for (let count = 0; count < notes.length; count++) {
+//         //console.log(notes[count]); // COUNT BECOMES THE INDEX(individual item) and we grab the item with its index using BRACKET NOTATION
+//         }; // loop through array
 
-        for (let count = notes.length -1; count >= 0; count--) {
-        //console.log(notes[count]); // count becomes index and we grab the item with its index using bracket notation
-        }; // loop through array in reverse
+//         for (let count = notes.length -1; count >= 0; count--) {
+//         //console.log(notes[count]); // count becomes index and we grab the item with its index using bracket notation
+//         }; // loop through array in reverse
 
-// SEARCHING Arrays PART 1 -----------------------------
+// SEARCHING Arrays PART 1  and PART 2 -----------------
+        
+        // // indexOf - returns an items position in array, if there are multiple, it returns the first one. if it doesn't exist, it returns a -1. Will always use === to find match. however, when searching objects, {} === {} = false. objects only equal other objects if they are exactly the same object. Use indexOf for an array of STRINGS
+        // notes.indexOf();  
+
+        // // findIndex - allows you to work with an array of objects. Finds first match and stops running otherwise returns -1 if item not found. Use findIndex for an array of OBJECTS 
+        // const indexOfNote = notes.findIndex(function (note, index) {
+        //         return note.title === "Habbits to work on";
+        // });
+        // // console.log(indexOfNote);
+
+        // const findNote = function (thing, noteTitle) {
+        //         const index = thing.findIndex(function (note, index) {
+        //                 return note.title.toUpperCase() === noteTitle.toUpperCase()
+        //         });
+                
+        //         return thing[index];
+        // };// is a function that returns the entire object in an array. Undefined if not in array of objects
+
+        // const theNote = findNote(notes, "office modifications");
+        // // console.log(theNote);
+
+        // //FIND - works in a similar way to function above, however, it returns the value of the first element found instead of returning an index
+        // const foundNote = function (thing, noteTitle) {
+        //         return thing.find(function (note, index) {
+        //                 return note.title.toUpperCase() === noteTitle.toUpperCase()
+        //         });
+        // };
+
+        // const found = foundNote(notes, "office modifications");
+        // // console.log(found);
+
+        // // alt 
+        // const foundTheNote = function (thing, noteTitle) {
+        //         const note = thing.find(function (note, index) {
+        //                 return note.title.toUpperCase() === noteTitle.toUpperCase()
+        //         });
+        //         return note
+        // };
+
+        // const founds = foundTheNote(notes, "office modifications");
+        // console.log(founds);
 
 // FILTERING -------------------------------------------
+
+        // FILTER - creates new array with all elements that pass the test implemented by the provided function
+        const filteredNotes = notes.filter(function (note, index) {
+                const isTitleMatch = note.title.toLowerCase().includes("ne")// this is called method chaining
+                const isBodyMatch = note.body.toLowerCase().includes("ne")
+                        return isTitleMatch || isBodyMatch
+        });
+
+        //console.log(filteredNotes)
+
+        const findNotes = function (notes, query) {
+                return notes.filter(function (note, index) {
+                        const isTitleMatch = note.title.toUpperCase().includes(query.toUpperCase())
+                        const isBodyMatch = note.body.toUpperCase().includes(query.toUpperCase())
+                                return isTitleMatch || isBodyMatch
+                });
+        };
+
+        console.log(findNotes(notes, "work"))
 
 // SORTING ---------------------------------------------
 
--- Renewed Attempt 10-29-18/Arrays/todo.js
+++ Renewed Attempt 10-29-18/Arrays/todo.js
@@ -1,34 +1,92 @@
-// Challenge Section
-const todo = ["Buy pet food", "Walk the dog", "Study", "Exercise", "Wash clothes"];
+// CHALLENGE SECTION
+const todo = [{
+    text: "Buy pet food",
+    completed: true
+}, {
+    text: "Walk the dog",
+    completed: false
+}, {
+    text: "Study",
+    completed: true
+}, {
+    text: "Exercise",
+    completed: false
+}, {
+    text: "Wash clothes",
+    completed: false
+}];
 
-// Challenge #1 - print 'you have "X" todos', first and second to last items
+// CHALLENGE #1 - print 'you have "X" todos', first and second to last items
 
     // console.log(`You have ${todo.length} things to do.`);
     // console.log(`Todo #1: ${todo[0]} and Todo #4: ${todo[todo.length - 2]}`);
 
-// Challenge #2 - delete 3rd item, add new item to end, and remove first item
+// CHALLENGE #2 - delete 3rd item, add new item to end, and remove first item
 
-    todo.splice(2, 1);
-    todo.push("Take a shower");
-    todo.shift();
+//     todo.splice(2, 1);
+//     todo.push("Take a shower");
+//     todo.shift();
 
-// Challenge #3 - use for each, 1. first item, 2. second item, etc
+// // CHALLENGE #3 - use for each, 1. first item, 2. second item, etc
 
-    todo.forEach(function (item, index) {
-        const position = index + 1;
-        // console.log(`${position}. ${item}`);
-    });
+//     todo.forEach(function (item, index) {
+//         const position = index + 1;
+//         // console.log(`${position}. ${item}`);
+//     });
 
-// Challenge #4 - re-create forEach outcome with a for loop
+// // CHALLENGE #4 - re-create forEach outcome with a for loop
 
-    for (let count = 0; count < todo.length; count++) {
-        //console.log(`${count + 1}. ${todo[count]}`);
+//     for (let count = 0; count < todo.length; count++) {
+//         //console.log(`${count + 1}. ${todo[count]}`);
+//     };
+
+// CHALLENGE #5 - convert array to array of objects, add text and completed property, create function that removes todo by text value
+
+    // const deleteTodo = function (todoList, todoText) { 
+    //     const item = todoList.find(function (todo, index) {
+    //        return todo.text.toUpperCase() === todoText.toUpperCase()
+    //     });
+    //     return item.shift()
+    // }; 
+
+   
+    // deleteTodo(todo, "exercise"); // FAILED ATTEMPT
+    // console.log(todo); 
+
+    // SOLUTION 
+    // const deleteTodo2 = function (todoList, todoText) {
+    //     const index = todoList.findIndex(function (todo) {
+    //         return todo.text.toUpperCase() === todoText.toUpperCase()
+    //     });
+
+    //     if (index > -1) {// if item exists
+    //         todoList.splice(index, 1) // delete item by index found with above method (in this case, findIndex)
+    //     };
+    // };
+
+    // deleteTodo2(todo, "exercise");
+    // console.log(todo);
+
+// CHALLENGE #6 - 
+
+    // const getThingsTodo = function (todos) {
+    //     return todos.filter(function (item, index) {
+    //         const unfinished = item.completed === false
+    //             return unfinished
+    //     });
+    // };
+
+    // console.log(getThingsTodo(todo))
+
+    // alt solution(s)
+    const getThingsTodo = function (todos) {
+        return todos.filter(function (item, index) {
+            return !item.completed
+        });
     };
 
-// Challenge #5 - 
+    console.log(getThingsTodo(todo))
 
-// Challenge #6 - 
-
-// Challenge #7 - 
+// CHALLENGE #7 - 
 
     
