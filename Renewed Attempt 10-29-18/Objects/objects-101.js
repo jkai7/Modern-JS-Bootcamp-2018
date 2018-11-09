bd97829a4ac8cabd65d03ae8c2f280cb5409b75f
Jens Kai <jkaih7@gmail.com>
2018-11-09 01:10:14 -0500

    set up note app

--- Renewed Attempt 10-29-18/Objects/objects-101.js
+++ Renewed Attempt 10-29-18/Objects/objects-101.js
@@ -4,24 +4,25 @@ let myBook = {
     pageCount: 326
 };
 
-console.log(`${myBook.title} by ${myBook.author}`);
+// console.log(`${myBook.title} by ${myBook.author}`);
 
-myBook.title = "Animal Farm";
+// myBook.title = "Animal Farm";
 
-console.log(`${myBook.title} by ${myBook.author}`);
+// console.log(`${myBook.title} by ${myBook.author}`);
 
-//= challenge - model a person
+// //= challenge - model a person
 
-let person = {
-    name: "Jens",
-    age: 31,
-    location: "Miami"
-};
+// let person = {
+//     name: "Jens",
+//     age: 31,
+//     location: "Miami"
+// };
 
-console.log(`${person.name} is ${person.age} and lives in ${person.location}`);
+// console.log(`${person.name} is ${person.age} and lives in ${person.location}`);
 
-person.age = person.age + 1;
+// person.age = person.age + 1;
 
-console.log(`${person.name} is ${person.age} and lives in ${person.location}`);
+// console.log(`${person.name} is ${person.age} and lives in ${person.location}`);
 
 
+console.log(myBook.title)
